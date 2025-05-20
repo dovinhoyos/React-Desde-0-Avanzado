8d603145617a2cfbd319b3816243b254e7a6aa52
dovinhoyos <dovricarbhoy@gmail.com>
2025-05-20 10:49:23 -0500

    refactor: simplify Button component and enhance ColorRed functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { useFetch } from "./hooks";
 import "./App.css";
-
-const url = "https://api.example.com/data";
-// const urlUser = "https://api.example.com/user";
-
-interface Data {
-  name: string;
-  lastname: string;
-  age: number;
-}
+import { Button, ColorRed } from "./components";
 
 function App() {
-  const { data, loading, error } = useFetch<Data>(url);
-  // const { data: dataUser, loading: loadingUser, error: errorUser } = useFetch<{name: string}>(url);
-
-  if (loading) {
-    return <p>Loading........</p>;
-  }
-
-  if (error) {
-    return <p>Ups! hay un error: {error.message}</p>;
-  }
-
+  const handleClick = () => {
+    alert("Hola!");
+  };
   return (
     <>
-      <p>{JSON.stringify(data)}</p>
+      <Button parentMethod={handleClick}>Clickeame papu!</Button>
+      <ColorRed>
+        <Button parentMethod={handleClick}>Clickeame papu!</Button>
+      </ColorRed>
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,23 @@
-import './Button.css'
+import { ReactNode } from "react";
+import "./Button.css";
 
 interface Props {
-    label: string;
-    parentMethod: () => void;
+  children: ReactNode;
+  parentMethod: () => void;
 }
 
-export const Button = ({ label, parentMethod }: Props) => {
-    return (
-        <button className="custom-button" onClick={parentMethod}>
-            {label}
-        </button>
-    )
+interface ChildrenProps {
+  children: ReactNode;
 }
+
+export const ColorRed = ({ children }: ChildrenProps) => {
+  return <div className="color-red">{children}</div>;
+};
+
+export const Button = ({ children, parentMethod }: Props) => {
+  return (
+    <button className="custom-button" onClick={parentMethod}>
+      {children}
+    </button>
+  );
+};
